b16be43e46ef11a1088502bd843cce04afb97869
sachins32 <sachinph32@gmail.com>
2023-08-15 16:42:48 +0530

    Remove unnecessary imports

--- src/main/java/com/sachin/StudentAPI/service/StudentService.java
+++ src/main/java/com/sachin/StudentAPI/service/StudentService.java
@@ -3,8 +3,6 @@ package com.sachin.StudentAPI.service;
 import com.sachin.StudentAPI.model.Student;
 import com.sachin.StudentAPI.repository.StudentRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
